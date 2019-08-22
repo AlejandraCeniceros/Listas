2fffdff3a0ad7c2b94f29760808d1caf8eaa7818
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-22 08:40:32 -0700

    Agregar archivos de proyecto.

---
+++ Listas/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> colores = new List<string>();
+            colores.Add("Rojo");
+            colores.Add("Verde");
+            colores.Add("Naranja");
+            colores.Add("Amarillo");
+            colores.Add("Azul");
+
+            colores.RemoveAt(1);
+            colores.Remove("Amarillo");
+
+            Console.WriteLine("Numero de elementos:" + colores.Count);
+
+            int indiceColor = 1;
+            foreach(var color in colores)
+            {
+                Console.WriteLine("elemento:" + indiceColor.ToString() + ":" + color);
+                indiceColor++;
+            }
+
+            Console.Read();
+
+        }
+    }
+}
